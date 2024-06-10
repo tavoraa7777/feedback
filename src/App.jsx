36bd0faf3065b514e84c43f6887a512ada6258d1
tavoraa 7777 <guilermetavora05@gmail.com>
2024-06-10 01:56:59 +0000

    feat: instalaçao e implantaçao do styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 
+import { Home } from "./pages/Home"
+import { theme } from "./styles/theme.js"
 
-export default function App() {
+const router = createBrowserRouter ([
+
+  {
+    path: "/",
+    element: <Home /> 
+
+  }
+
+])
+
+export function App() {
   return (
-    <div>App</div>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
